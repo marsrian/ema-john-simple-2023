4ecb8f10fc89b939cda5e9bf066e5fab0933aaf4
marsrian <marsrian40@gmail.com>
2023-03-27 15:26:39 +0600

    Cart section added

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    // console.log(props);
+    const {name, img, seller, quantity, ratings, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Ratings: {ratings} Stars</p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
+        </div>
+    );
+};
+
+export default Product;
