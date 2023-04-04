6c02eff51a0fa2e0c52b25ccc49fbe05fc52e22a
marsrian <marsrian40@gmail.com>
2023-04-04 23:35:49 +0600

    implement delete from cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -20,7 +20,7 @@ const Product = (props) => {
             <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
                 Add to Cart
                 <FontAwesomeIcon icon={faShoppingCart} />
-                </button>
+            </button>
         </div>
     );
 };
